308d4edb9220a440c12401d39469255723996cbe
Magnus Tornquist <dt08mt0@val-1.student.lth.se>
2015-04-20 15:20:01 +0200

    oops

--- StreamingExample/DecodeAndPlayVideo.java
+++ StreamingExample/DecodeAndPlayVideo.java
@@ -1,4 +1,133 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2010 Xuggle Inc.  All rights reserved.
+ *  
+ * This file is part of Xuggle-Xuggler-Main.
+ *
+ * Xuggle-Xuggler-Main is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Xuggle-Xuggler-Main is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Xuggle-Xuggler-Main.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
 
+package found;
+
+import java.awt.image.BufferedImage;
+
+import com.xuggle.xuggler.Global;
+import com.xuggle.xuggler.IContainer;
+import com.xuggle.xuggler.IPacket;
+import com.xuggle.xuggler.IPixelFormat;
+import com.xuggle.xuggler.IStream;
+import com.xuggle.xuggler.IStreamCoder;
+import com.xuggle.xuggler.ICodec;
+import com.xuggle.xuggler.IVideoPicture;
+import com.xuggle.xuggler.IVideoResampler;
+import com.xuggle.xuggler.Utils;
+import com.xuggle.xuggler.demos.VideoImage;
+
+/**
+ * Takes a media container, finds the first video stream, decodes that stream,
+ * and then displays the video frames, at the frame-rate specified by the
+ * container, on a window.
+ * 
+ * @author aclarke
+ * 
+ */
+public class DecodeAndPlayVideo {
+
+	/**
+	 * Takes a media container (file) as the first argument, opens it, opens up
+	 * a Swing window and displays video frames with <i>roughly</i> the right
+	 * timing.
+	 * 
+	 * @param args
+	 *            Must contain one string which represents a filename
+	 */
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+		// if (args.length <= 0)
+		// throw new IllegalArgumentException("must pass in a filename" +
+		// " as the first argument");
+
+		// String filename = args[0];
+		String filename = "movie.mp4";
+		// Let's make sure that we can actually convert video pixel formats.
+		if (!IVideoResampler
+				.isSupported(IVideoResampler.Feature.FEATURE_COLORSPACECONVERSION))
+			throw new RuntimeException("you must install the GPL version"
+					+ " of Xuggler (with IVideoResampler support) for "
+					+ "this demo to work");
+
+		// Create a Xuggler container object
+		IContainer container = IContainer.make();
+
+		// Open up the container
+		if (container.open(filename, IContainer.Type.READ, null) < 0)
+			throw new IllegalArgumentException("could not open file: "
+					+ filename);
+
+		// query how many streams the call to open found
+		int numStreams = container.getNumStreams();
+
+		// and iterate through the streams to find the first video stream
+		int videoStreamId = -1;
+		IStreamCoder videoCoder = null;
+		for (int i = 0; i < numStreams; i++) {
+			// Find the stream object
+			IStream stream = container.getStream(i);
+			// Get the pre-configured decoder that can decode this stream;
+			IStreamCoder coder = stream.getStreamCoder();
+
+			if (coder.getCodecType() == ICodec.Type.CODEC_TYPE_VIDEO) {
+				videoStreamId = i;
+				videoCoder = coder;
+				break;
+			}
+		}
+		if (videoStreamId == -1)
+			throw new RuntimeException(
+					"could not find video stream in container: " + filename);
+
+		/*
+		 * Now we have found the video stream in this file. Let's open up our
+		 * decoder so it can do work.
+		 */
+		if (videoCoder.open() < 0)
+			throw new RuntimeException(
+					"could not open video decoder for container: " + filename);
+
+		IVideoResampler resampler = null;
+		if (videoCoder.getPixelType() != IPixelFormat.Type.BGR24) {
+			// if this stream is not in BGR24, we're going to need to
+			// convert it. The VideoResampler does that for us.
+			resampler = IVideoResampler.make(videoCoder.getWidth(),
+					videoCoder.getHeight(), IPixelFormat.Type.BGR24,
+					videoCoder.getWidth(), videoCoder.getHeight(),
+					videoCoder.getPixelType());
+			if (resampler == null)
+				throw new RuntimeException("could not create color space "
+						+ "resampler for: " + filename);
+		}
+		/*
+		 * And once we have that, we draw a window on screen
+		 */
+		openJavaWindow();
+
+		/*
+		 * Now, we start walking through the container looking at each packet.
+		 */
+		IPacket packet = IPacket.make();
+		long firstTimestampInStream = Global.NO_PTS;
+		long systemClockStartTime = 0;
+		while (container.readNextPacket(packet) >= 0) {
 			/*
 			 * Now we have a packet, let's see if it belongs to our video stream
 			 */
@@ -123,4 +252,47 @@
 				} while (false);
 			}
 
-		
+		}
+		/*
+		 * Technically since we're exiting anyway, these will be cleaned up by
+		 * the garbage collector... but because we're nice people and want to be
+		 * invited places for Christmas, we're going to show how to clean up.
+		 */
+		if (videoCoder != null) {
+			videoCoder.close();
+			videoCoder = null;
+		}
+		if (container != null) {
+			container.close();
+			container = null;
+		}
+		closeJavaWindow();
+
+	}
+
+	/**
+	 * The window we'll draw the video on.
+	 * 
+	 */
+	private static VideoImage mScreen = null;
+
+	private static void updateJavaWindow(BufferedImage javaImage) {
+		mScreen.setImage(javaImage);
+	}
+
+	/**
+	 * Opens a Swing window on screen.
+	 */
+	private static void openJavaWindow() {
+		mScreen = new VideoImage();
+	}
+
+	/**
+	 * Forces the swing thread to terminate; I'm sure there is a right way to do
+	 * this in swing, but this works too.
+	 */
+	private static void closeJavaWindow() {
+		System.exit(0);
+	}
+}
+
