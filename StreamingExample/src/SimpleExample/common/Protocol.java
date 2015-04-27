53cc40189b5f2f6485f321c2136a16adc6bf59ad
arturmatulaniec <dat11ama@student.lu.se>
2015-04-27 14:32:38 +0200

    protokoll stuff

--- StreamingExample/src/SimpleExample/common/Protocol.java
+++ StreamingExample/src/SimpleExample/common/Protocol.java
@@ -7,11 +7,14 @@ public class Protocol {
 	// Server => Client
 	public static final byte FRAME_BEGIN = 10;
 	public static final byte STREAM_END = 11;
+	public static final byte LIST_START = 12; 
 	
 	// Client => Server
 	public static final byte CLOSE_STREAM = 30;
 	public static final byte PLAY_STREAM = 31;
-	public static final byte PAUSE_STREAM = 32; 
+	public static final byte PAUSE_STREAM = 32;
+	public static final byte GIVE_MOVIE_LIST = 33;
+	public static final byte CHOSEN_TITLE = 34;
 	
 	
 }
