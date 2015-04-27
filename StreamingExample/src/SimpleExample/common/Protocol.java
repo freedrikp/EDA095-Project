886badece9f352ba782edd11628ab8bee2f2a0a0
arturmatulaniec <dat11ama@student.lu.se>
2015-04-27 13:17:08 +0200

    lite förändring i protocol...

--- StreamingExample/src/SimpleExample/common/Protocol.java
+++ StreamingExample/src/SimpleExample/common/Protocol.java
@@ -9,7 +9,9 @@ public class Protocol {
 	public static final byte STREAM_END = 11;
 	
 	// Client => Server
-	public static final byte CLOSE_STREAM = 20;
+	public static final byte CLOSE_STREAM = 30;
+	public static final byte PLAY_STREAM = 30;
+	public static final byte PAUSE_STREAM = 31; 
 	
 	
 }
